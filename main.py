8052587916e06f2850c3c5591c16754171376df1
EitanKravi <eitan.almasi@gmail.com>
2026-08-16 19:43:36 +0300

    UPDATE 1.1: FPS mechanics were added to the game

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sys
-
 import pygame
 
 # take snapshot of the game state
@@ -11,12 +9,20 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
 
+    # create a Clock object
+    clock = pygame.time.Clock()
+
+    # create delta time
+    dt: float= 0.0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    # create a screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # main loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -25,9 +31,10 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        # update delta time for 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
     pygame.quit()
-
-
