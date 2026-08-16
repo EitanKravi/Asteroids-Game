59ac039186338946e552bd99b1a2304a37869415
EitanKravi <eitan.almasi@gmail.com>
2026-08-16 19:22:46 +0300

    Update 1.0: Added Git configuration, added a logger component to record the game state (snapshot), added constants for storage values, and created the main loop and game screen.

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import sys
+
 import pygame
 
-print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+# take snapshot of the game state
+from logger import log_state
+
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+def main():
+    pygame.init()
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
+    pygame.quit()
 
 
